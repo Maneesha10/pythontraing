fbf6c20234d84f5c87e2397d634a9dc1bb05f80f
boddu <boddu.maneesha@sonata_software.com>
2021-09-30 16:01:24 +0530

    files commited

--- hii.py
+++ hii.py
@@ -26,3 +26,12 @@ print(Days)
 
 Days.discard("Mon")
 print(Days)
+
+#tuples
+tup1 = ('physics', 'chemistry', 1997, 2000);
+tup2 = (1, 2, 3, 4, 5, 6, 7 );
+print ("tup1[0]: ", tup1[0]);
+print ("tup2[1:5]: ", tup2[1:5]);
+
+tup3 = tup1 + tup2;
+print(tup3);
